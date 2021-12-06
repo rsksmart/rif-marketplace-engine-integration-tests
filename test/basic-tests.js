4c4525d6573c01c32d2c997ee2d991540450801e
Juan Techera <juanjose@rsk.co>
2021-12-06 11:23:12 -0300

    WIP

--- test/basic-tests.js
+++ test/basic-tests.js
@@ -1,9 +1,20 @@
 const assert = require('chai').assert;
+const MarketPlaceData = require('./build/contracts/MarketPlace.json');
+import {
+    Contract,
+    Wallet
+} from 'ethers';
+
 describe(`MarketPlace Engine Integration Tests`, function () {
-    before(async () => {
-
-
-    });
+    beforeEach(() => {
+            marketplaceContract = MarketplaceContract.getInstance(
+                MarketPlaceData.networks[33].address,
+                new Wallet(
+                    '0xc85ef7d79691fe79573b1a7064c19c1a9819ebdbd1faaab1a8ec92344438aaf4'
+                )
+            );
+        }
+    };
 
 
     it('First test', async () => {
