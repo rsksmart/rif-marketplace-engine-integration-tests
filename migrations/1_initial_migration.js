cbb738e7c79589d325be32fc299a74dd7b225f21
Juan Techera <juanjose@rsk.co>
2021-12-16 13:07:15 -0300

    WIP

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const MarketPlace = artifacts.require("Marketplace");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(MarketPlace,true,true,true,true,true,true,true);
 };
--- test/basic-tests.js
+++ test/basic-tests.js
@@ -6,32 +6,28 @@ const Contract = ethers.Contract;
 const Wallet = ethers.Wallet;
 
 const chaiAsPromised = require("chai-as-promised");
+let provider;
+let marketplaceContract;
 
-const marketplaceContract = new Contract();
+describe('MarketPlace Engine Integration Tests', async () => {
 
-describe(`MarketPlace Engine Integration Tests`, function () {
-    beforeEach(() => {
-        marketplaceContract = MarketplaceContract.getInstance(
-            MarketPlaceData.networks[33].address,
-            new Wallet(
-                '0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826'
-            )
-        );
-    });
+  before(async () => {
 
-    it('should create a single marketplace contract instance', async () => {
-        // Unit under test
-        const marketplaceContractInstance = MarketplaceContract.getInstance(
-            MarketPlaceData.networks[33].address,
-          new Wallet(
-            '0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826'
-          )
-        );
-    
-        // Verify results
-        expect(marketplaceContract).to.be.equal(marketplaceContractInstance);
-      });
+    let url = "http://127.0.0.1:4444";
+    provider = new ethers.providers.StaticJsonRpcProvider(url);
+    const signer = provider.getSigner();
+    marketplaceContract = new ethers.Contract(MarketPlaceData.networks[33].address, MarketPlaceData.abi, provider);
+console.log(JSON.stringify(marketplaceContract));
+  });
+  beforeEach(() => {
+
+  });
+
+  it('should create a single marketplace contract instance', async () => {
+    // Unit under test
 
 
+    // Verify results
+  });
 
 });
