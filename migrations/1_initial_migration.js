5a9b3eb7a463146c4da4efca67536c1e8faec748
Ricardo Jauregui Yokokura <ricardo@iovlabs.org>
2021-12-28 12:34:57 -0500

    config test cases

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,7 @@
-const Migrations = artifacts.require("Migrations");
-const MarketPlace = artifacts.require("Marketplace");
+const Migrations = artifacts.require('Migrations');
+const MarketPlace = artifacts.require('Marketplace');
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(MarketPlace,true,true,true,true,true,true,true);
+  deployer.deploy(MarketPlace, true, true, true, true, true, true, true);
 };
--- src/basic-tests.test.ts
+++ src/basic-tests.test.ts
@@ -11,26 +11,78 @@ import deployedMarketPlaceData from '../build/contracts/Marketplace.json';
 chai.use(chaiAsPromised);
 const providerUrl = 'http://127.0.0.1:4444';
 
-describe('MarketPlace Engine Integration Tests', async () => {
-  it('should create a  marketplace contract instance', async () => {
-    // Unit under test
+describe('Marketplace Engine Integration Tests', async () => {
+  it('should create a marketplace contract instance', async () => {
     const marketplace = createMarketplaceContract(
       deployedMarketPlaceData.networks[33].address,
       new ethers.providers.StaticJsonRpcProvider(providerUrl)
     );
     expect(marketplace).not.to.be.undefined;
-    // Verify results
   });
 
-  it('should get the marketplace configuration for "assetValidation"', async () => {
-    // Unit under test
-    const marketplace = createMarketplaceContract(
-      deployedMarketPlaceData.networks[33].address,
-      new ethers.providers.StaticJsonRpcProvider(providerUrl)
-    );
+  describe('Marketplace config attributes', async () => {
+    it('should get true for marketplace configuration for "whitelistedAsset"', async () => {
+      const marketplace = createMarketplaceContract(
+        deployedMarketPlaceData.networks[33].address,
+        new ethers.providers.StaticJsonRpcProvider(providerUrl)
+      );
+      const config = createMarketplaceConfiguration(marketplace);
+      await expect(config.get('whitelistedAsset')).to.eventually.be.true;
+    });
 
-    const config = createMarketplaceConfiguration(marketplace);
-    await expect(config.get('assetValidation')).to.eventually.be.rejected;
-    // Verify results
+    it('should get true for marketplace configuration for "whitelistedAssetProvider"', async () => {
+      const marketplace = createMarketplaceContract(
+        deployedMarketPlaceData.networks[33].address,
+        new ethers.providers.StaticJsonRpcProvider(providerUrl)
+      );
+      const config = createMarketplaceConfiguration(marketplace);
+      await expect(config.get('whitelistedAssetProvider')).to.eventually.be
+        .true;
+    });
+
+    it('should get true for marketplace configuration for "assetValidation"', async () => {
+      const marketplace = createMarketplaceContract(
+        deployedMarketPlaceData.networks[33].address,
+        new ethers.providers.StaticJsonRpcProvider(providerUrl)
+      );
+      const config = createMarketplaceConfiguration(marketplace);
+      await expect(config.get('assetValidation')).to.eventually.be.true;
+    });
+
+    it('should get true for marketplace configuration for "saleStrategyPerAsset"', async () => {
+      const marketplace = createMarketplaceContract(
+        deployedMarketPlaceData.networks[33].address,
+        new ethers.providers.StaticJsonRpcProvider(providerUrl)
+      );
+      const config = createMarketplaceConfiguration(marketplace);
+      await expect(config.get('saleStrategyPerAsset')).to.eventually.be.true;
+    });
+
+    it('should get true for marketplace configuration for "sameCurrencyPerAsset"', async () => {
+      const marketplace = createMarketplaceContract(
+        deployedMarketPlaceData.networks[33].address,
+        new ethers.providers.StaticJsonRpcProvider(providerUrl)
+      );
+      const config = createMarketplaceConfiguration(marketplace);
+      await expect(config.get('sameCurrencyPerAsset')).to.eventually.be.true;
+    });
+
+    it('should get true for marketplace configuration for "assetSubdomain"', async () => {
+      const marketplace = createMarketplaceContract(
+        deployedMarketPlaceData.networks[33].address,
+        new ethers.providers.StaticJsonRpcProvider(providerUrl)
+      );
+      const config = createMarketplaceConfiguration(marketplace);
+      await expect(config.get('assetSubdomain')).to.eventually.be.true;
+    });
+
+    it('should get true for marketplace configuration for "stakingAndSlashing"', async () => {
+      const marketplace = createMarketplaceContract(
+        deployedMarketPlaceData.networks[33].address,
+        new ethers.providers.StaticJsonRpcProvider(providerUrl)
+      );
+      const config = createMarketplaceConfiguration(marketplace);
+      await expect(config.get('stakingAndSlashing')).to.eventually.be.true;
+    });
   });
 });
