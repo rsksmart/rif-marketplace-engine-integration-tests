b62965649bac597b43b7a8a11decb6addc20f3eb
Ricardo Jauregui Yokokura <ricardo@iovlabs.org>
2022-01-19 19:53:15 -0500

    test(qa): adding marketplace config test cases

--- test/marketplace/configuration.test.ts
+++ test/marketplace/configuration.test.ts
@@ -1,11 +1,25 @@
 import chai, { expect } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import { createMarketplaceContract } from 'rif-marketplace-engine-sdk';
-import { Contract } from 'ethers';
+import {
+  createMarketplaceConfiguration,
+  createMarketplaceContract,
+} from 'rif-marketplace-engine-sdk';
+import { Contract, ContractTransaction, Signer } from 'ethers';
+import { Provider } from '@ethersproject/providers';
+import { Marketplace } from 'rif-marketplace-engine/typechain';
 import {
   assertMarketplaceConfiguration,
+  getMarketplaceConfiguration,
   prepareMarkerplace,
 } from './configuration.utils';
+
+export type MarketplaceSetUp = {
+  deployer: Signer;
+  otherSigners: Signer[];
+  provider: Provider;
+  marketplace: Marketplace;
+  marketplaceAddress: string;
+};
 
 chai.use(chaiAsPromised);
 
@@ -35,6 +49,142 @@ describe('Marketplace Configuration', () => {
       requireSameCurrencyPerAsset: true,
       requireAssetSubdomain: true,
       supportStakingAndSlashing: true,
+    });
+  });
+
+  describe('Getting & setting marketplace configurations', () => {
+    describe('Getter', () => {
+      // Tests that marketplace has the right configuration at deployment
+      getMarketplaceConfiguration();
+    });
+
+    describe('Setter', () => {
+      // Tests that marketplace has the right configuration at deployment
+      it('should get true when "whitelistedAsset" attribute is set enabled', async () => {
+        const { deployer, marketplaceAddress } = await prepareMarkerplace();
+
+        const marketplaceInstance = createMarketplaceContract(
+          marketplaceAddress,
+          deployer
+        );
+        const marketplaceConfig =
+          createMarketplaceConfiguration(marketplaceInstance);
+        const tx = (await marketplaceConfig.set(
+          'whitelistedAsset',
+          true
+        )) as ContractTransaction;
+        await tx.wait();
+        await expect(marketplaceConfig.get('whitelistedAsset')).to.eventually.be
+          .true;
+      });
+
+      it('should get true when "whitelistedAssetProvider" attribute is set enabled', async () => {
+        const { deployer, marketplaceAddress } = await prepareMarkerplace();
+
+        const marketplaceInstance = createMarketplaceContract(
+          marketplaceAddress,
+          deployer
+        );
+        const marketplaceConfig =
+          createMarketplaceConfiguration(marketplaceInstance);
+        const tx = (await marketplaceConfig.set(
+          'whitelistedAssetProvider',
+          true
+        )) as ContractTransaction;
+        await tx.wait();
+        await expect(marketplaceConfig.get('whitelistedAssetProvider')).to
+          .eventually.be.true;
+      });
+
+      it('should get true when "assetValidation" attribute is set enabled', async () => {
+        const { deployer, marketplaceAddress } = await prepareMarkerplace();
+
+        const marketplaceInstance = createMarketplaceContract(
+          marketplaceAddress,
+          deployer
+        );
+        const marketplaceConfig =
+          createMarketplaceConfiguration(marketplaceInstance);
+        const tx = (await marketplaceConfig.set(
+          'assetValidation',
+          true
+        )) as ContractTransaction;
+        await tx.wait();
+        await expect(marketplaceConfig.get('assetValidation')).to.eventually.be
+          .true;
+      });
+
+      it('should get true when "saleStrategyPerAsset" attribute is set enabled', async () => {
+        const { deployer, marketplaceAddress } = await prepareMarkerplace();
+
+        const marketplaceInstance = createMarketplaceContract(
+          marketplaceAddress,
+          deployer
+        );
+        const marketplaceConfig =
+          createMarketplaceConfiguration(marketplaceInstance);
+        const tx = (await marketplaceConfig.set(
+          'saleStrategyPerAsset',
+          true
+        )) as ContractTransaction;
+        await tx.wait();
+        await expect(marketplaceConfig.get('saleStrategyPerAsset')).to
+          .eventually.be.true;
+      });
+
+      it('should get true when "sameCurrencyPerAsset" attribute is set enabled', async () => {
+        const { deployer, marketplaceAddress } = await prepareMarkerplace();
+
+        const marketplaceInstance = createMarketplaceContract(
+          marketplaceAddress,
+          deployer
+        );
+        const marketplaceConfig =
+          createMarketplaceConfiguration(marketplaceInstance);
+        const tx = (await marketplaceConfig.set(
+          'sameCurrencyPerAsset',
+          true
+        )) as ContractTransaction;
+        await tx.wait();
+        await expect(marketplaceConfig.get('sameCurrencyPerAsset')).to
+          .eventually.be.true;
+      });
+
+      it('should get true when "assetSubdomain" attribute is set enabled', async () => {
+        const { deployer, marketplaceAddress } = await prepareMarkerplace();
+
+        const marketplaceInstance = createMarketplaceContract(
+          marketplaceAddress,
+          deployer
+        );
+        const marketplaceConfig =
+          createMarketplaceConfiguration(marketplaceInstance);
+        const tx = (await marketplaceConfig.set(
+          'assetSubdomain',
+          true
+        )) as ContractTransaction;
+        await tx.wait();
+        await expect(marketplaceConfig.get('assetSubdomain')).to.eventually.be
+          .true;
+      });
+
+      it('should get true when "stakingAndSlashing" attribute is set enabled', async () => {
+        const { deployer, marketplaceAddress } = await prepareMarkerplace();
+
+        const marketplaceInstance = createMarketplaceContract(
+          marketplaceAddress,
+          deployer
+        );
+        const marketplaceConfig =
+          createMarketplaceConfiguration(marketplaceInstance);
+        const tx = (await marketplaceConfig.set(
+          'stakingAndSlashing',
+          true
+        )) as ContractTransaction;
+        await tx.wait();
+        await expect(marketplaceConfig.get('stakingAndSlashing')).to.eventually
+          .be.true;
+      });
     });
   });
 });
